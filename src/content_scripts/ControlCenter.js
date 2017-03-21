967b3ca07713d5d11d238e9f3f787ae8a96d93f8
Stephan Spindler <spind42@gmail.com>
2017-03-21 03:55:52 +0100

    work commit  begonnen statistik pro MA zu schreiben

---
+++ src/content_scripts/ControlCenter.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  console.log("ControlCenter content script called");
+  var inputfield = $("#m_ddlEmployee");
+
+  //makeEmployeeSearchField(inputfield);
+
+});
--- src/content_scripts/tools.js
+++ src/content_scripts/tools.js
@@ -1,70 +1,111 @@
-
-
-
 function getKuerzel() {
-  console.log("getKuerzel");
+    console.log("getKuerzel");
     return new Promise(function(resolve, reject) {
-      chrome.storage.sync.get(STORAGE_KEY_KUERZEL, function(item) {
-          if (STORAGE_KEY_KUERZEL in item) {
-            resolve(item[STORAGE_KEY_KUERZEL]);
-          } else {
-            resolve("");
-          }
-      });
+        chrome.storage.sync.get(STORAGE_KEY_KUERZEL, function(item) {
+            if (STORAGE_KEY_KUERZEL in item) {
+                resolve(item[STORAGE_KEY_KUERZEL]);
+            } else {
+                resolve("");
+            }
+        });
     });
 
     //var username = chrome.sync.getItem("niuUsername");
     //var STORAGE_KEY_NIU_USERNAME = "niuUsername";
-/*
-    if (username) {
-        var res = username.split(" ");
-        var kuerzel = res[0].substring(0, 2) + res[1].substring(0, 2);
-        return kuerzel + " ";
-    } else {
-        return null;
-    } */
+    /*
+        if (username) {
+            var res = username.split(" ");
+            var kuerzel = res[0].substring(0, 2) + res[1].substring(0, 2);
+            return kuerzel + " ";
+        } else {
+            return null;
+        } */
 }
 
+function makeEmployeeSearchField(input, overlay) {
+  var names = [];
+  //datenstruktur für suche erzeugen
 
-function makeEmployeeSearchField(input) {
-    var names = [];
-    //datenstruktur für suche erzeugen
+  input.find("option").each(function(index, option) {
+      var option = $(option);
+      names.push(option.text());
+      console.log("collecting name " + option.text());
+  });
 
-    input.find("option").each(function(index, option) {
-        var option = $(option);
-        names.push(option.text());
-        console.log("collecting name " + option.text());
-    });
+  input.after("<input id='person_autocomplete'></input>");
+  $("#person_autocomplete").autocomplete({
+      source: names,
 
-    input.after("<input id='person_autocomplete'></input>");
-    $("#person_autocomplete").autocomplete({
-        source: names,
+  });
+  //input.hide(); //zumindest solange keine Lösung für FRAMEPROBLEM!!!
 
-    });
-    input.hide(); //zumindest solange keine Lösung für FRAMEPROBLEM!!!
+  /*
+  $("#person_autocomplete").on("autocompleteselect", function(event, ui) {
+      console.log("aut " + ui.item.value);
 
-    $("#person_autocomplete").on("autocompleteselect", function(event, ui) {
-        console.log("aut " + ui.item.value);
+      input.find("option").removeAttr("selected");
+      input.find("option:contains(" + ui.item.value + ")").attr("selected", "selected");
 
-        input.find("option").removeAttr("selected");
-        input.find("option:contains(" + ui.item.value + ")").attr("selected", "selected");
+  });
+  $("#person_autocomplete").on("autocompleteopen", function(event, ui) {
+      console.log("on open");
+      $(this).autocomplete('widget').css('z-index', 100);
+  });
+  */
 
-    });
-    $("#person_autocomplete").on("autocompleteopen", function(event, ui) {
-        console.log("on open");
-        $(this).autocomplete('widget').css('z-index', 100);
-
-
-        $("frame[name='ControlCenterMain']");
-        var main = $("frame[name='ControlCenterMain']"); //.children().css('opacity', "0.5");
-        console.log("main " + main);
-
-        main.remove();
-
-
-        console.log("frameset" + $("frameset"));
-        $("frameset").attr("rows", "100,25,*");
-
-    });
 
 };
+
+function makeEmployeeSearchField(input) {
+
+  makeEmployeeSearchField(input, false);
+
+};
+
+/*
+ * verwandelt die dienstnummer in die EmployeeId von NIU,
+ * damit ist es dann möglich direkt auf die Employee Page zuzugreifen
+ */
+function dnrToIdentifier(dnr) {
+    return new Promise(function(resolve, reject) {
+        var post = {};
+
+
+        $.get("https://niu.wrk.at/Kripo/external/ControlCenterHead.aspx", function(data) {
+            console.log("rcv from get ControlCenterHead.aspx" + data);
+            var jData = $(data);
+
+            var keyPostfix = jData.find("#__KeyPostfix").val();
+            var eventvalidation = jData.find("#__EVENTVALIDATION").val(); //jData.find("input[name=__EVENTVALIDATION]").val();
+
+            post["__KeyPostfix"] = keyPostfix;
+            post["__EVENTVALIDATION"] = eventvalidation;
+            post["__VIEWSTATE"] = "";
+            post["__EVENTARGUMENT"] = "";
+
+            post["m_txtEmployeeNumber"] = dnr;
+            post["m_btSend"] = "Anfage Senden";
+
+            console.log("show post " + post["__EVENTVALIDATION"]);
+
+
+            $.ajax({
+                url: "https://niu.wrk.at/Kripo/external/ControlCenterHead.aspx",
+                data: post,
+                type: "POST",
+// PROBLEM hier bekomme ich einfach nicht den link auf die seiten zurück? möglich wg. falschen referer?
+//                headers: {
+//                    'Origin': "https://niu.wrk.at",
+//                    'Referer': "https://niu.wrk.at/Kripo/external/ControlCenterHead.aspx"
+//                },
+                success: function(data, status) {
+                    console.log("rcv status " + status);
+                    console.log("rcv data " + data);
+
+                    //data.getElementById("m_lbtEmployeesummary");
+                    resolve("hallo welt");
+                }
+            });
+        });
+    });
+}
