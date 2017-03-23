0d314d5d5cde09dd23e793b58551cca7a8208f55
Stephan Spindler <spind42@gmail.com>
2017-03-23 10:15:26 +0100

    Optionen erweitert und Layout verbessert

--- src/definitions.js
+++ src/definitions.js
@@ -1,3 +1,15 @@
+//Storage keys
+
 var STORAGE_KEY_KUERZEL = "niu_kuerzel";
 
+var STORAGE_KEY_SEARCH_COURSE_ALWAYS_SEARCH = "niu_search_course_always_search";
+
+var STORAGE_KEY_CACHE_ACTIVE = "addon_cache_active";
+var STORAGE_KEY_MAX_CACHE_TIME = "addon_cache_max_cache_time";
+
+var DEFAULT_SEARCH_COURSE_ALWAYS_SEARCH = true;
+
 var POUCHDB_DB_NAME = "niuhelperdb1";
+
+var DEFAULT_MAX_CACHE_TIME = 100 * 60 * 60 * 24; //24 Stunden cache zeit
+var DEFAULT_CACHE_ACTIVE = true;
