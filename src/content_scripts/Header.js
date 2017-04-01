26debbe416d628a3ea980d18971db06ca9cb01d8
Sebastian Kuttnig <sebastian.kuttnig@gmail.com>
2017-04-01 16:23:59 +0200

    Benutzer über aktive Erweiterung informieren

---
+++ src/content_scripts/Header.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function() {
+
+$("#pageTitle").after('<b><a target="_blank" style="color:white;border-bottom: 1px white dotted; text-decoration: none;" href="https://github.com/geraldbaeck/NIUsLittleHelper#readme">NIU\'s little helper ist derzeit aktiv.</a></b>');
+
+});
