0fe6207367c882fc02b60c0a5f347dd91a03e087
Gerald Bäck <gerald@baeck.at>
2018-03-09 13:48:12 +0100

    Beförderungen werden jetzt beim DekretAlarm berücksichtigt
    also new Notification

--- src/content_scripts/detailEmployee.js
+++ src/content_scripts/detailEmployee.js
@@ -6,20 +6,24 @@ $(document).ready(function() {
   chrome.storage.sync.get(load, function(item) {
     if (item[STORAGE_KEY_DEKRET_ALERT]) {
       var dekretAlarm = [];
-      $("#ctl00_main_m_Employee_m_ccPromotionDecorations_m_tblPromotionDecorationsMain span[id$='_m_DescriptionLabel']:contains('nicht ausgefolgt')").each(function() {
+      $("span[id$='_m_DescriptionLabel']:contains('nicht ausgefolgt')").each(function() {
         var dekretName = $(this).parent().parent().parent().parent().find("td").first().text();
         var dekretDatum = $(this).parent().parent().parent().parent().find("input").first().val();
         dekretAlarm.push("<b>" + dekretName + "</b> vom " + dekretDatum);
       });
       if(dekretAlarm.length>0) {
-        var modalDiv = '<div id="dekretAlert" class="modal"><h3>Dekrete noch nicht ausgefolgt:</h3><ul style="margin:10px 0px;list-style-position:inside;padding-left:.5em;">';
+        var modalDiv = '<ul style="margin:10px 0px;list-style-position:inside;padding-left:.5em;">';
         $.each(dekretAlarm, function() {
           modalDiv += '<li style="margin-bottom:10px;">' + this + "</li>";
         });
-        modalDiv += '</ul></div>';
-        $('body').append(modalDiv);
+        modalDiv += '</ul>';
+        new PNotify({
+          title: 'Dekrete noch nicht ausgefolgt:',
+          text: modalDiv,
+          type: "info",
+          width: "400px",
+        });
       }
-      $('#dekretAlert').modal();
     }
   });
 });
